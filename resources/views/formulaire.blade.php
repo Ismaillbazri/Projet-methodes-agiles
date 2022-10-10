59cc35bece3519e14d1f99baf17a7c7babfb7740
Ismail Lbazri <88779439+Ismaillbazri@users.noreply.github.com>
2022-10-10 14:41:08 +0200

    form

--- resources/views/formulaire.blade.php
+++ resources/views/formulaire.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Straight-fly-InfoVol</title>
+        <title>Straight-fly</title>
        
   <!-- Favicons -->
   <link href="img/favicon.png" rel="icon">
@@ -102,53 +102,120 @@
     </div>
   </section><!-- End Hero -->
 
-  <main id="main">
 
     <!-- ======= Featured Services Section ======= -->
     <section id="featured-services" class="featured-services">
       <div class="container" data-aos="fade-up">
       <div class="section-title">
-            <h3>Informations du <span>Vol</span></h3>
+            <h3>Réserver le <span>vol</span></h3>
           
           <p>Réservez votre vol dés maintenant !</p>
         </div>
-        <div class="row">
-        <table >
-                <thead>
-                    <tr>
-                        <td >Nom vol</td>
-                        <td>Départ</td>
-                        <td>Arrivée</td>
-                        <td>Compagnie</td>
-                        <td>Durée</td>
-                        <td>Date</td>
-                        <td>Prix</td>
-                    </tr>
-                </thead> 
-                <tbody>
-                    <tr>
-                        <td>Vol 1</td>
-                        <td>Marrakech</td>
-                        <td>Paris</td>
-                        <td>RoyalAirMaroc</td>
-                        <td>3h</td>
-                        <td>14/01/2024</td>
-                        <td>300  €</td>
-                        <td><a href="{{route('réserver')}}"><button type="submit">Reserver</button></a></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-
+        <div class="falign-items-center">
+        <form class="needs-validation" novalidate>
+  <div class="form-row">
+    <div class="col-md-4 mb-3">
+      <label for="validationCustom01">First name</label>
+      <input type="text" class="form-control" id="validationCustom01" placeholder="First name" required>
+      <div class="valid-feedback">
+        Looks good!
       </div>
+    </div>
+
+    <div class="col-md-4 mb-3">
+      <label for="validationCustom02">Last name</label>
+      <input type="text" class="form-control" id="validationCustom02" placeholder="Last name" required>
+      <div class="valid-feedback">
+        Looks good!
+      </div>
+    </div>
+    <div class="form-row">
+    <div class="col-md-4 mb-3">
+      <label for="validationCustom01">Email</label>
+      <input type="text" class="form-control" id="validationCustom01" placeholder="Email" required>
+      <div class="valid-feedback">
+        Looks good!
+      </div>
+    </div>
+    
+  </div>
+  <div class="form-row">
+    <div class="col-md-6 mb-3">
+      <label for="validationCustom03">City</label>
+      <input type="text" class="form-control" id="validationCustom03" placeholder="City" required>
+      <div class="invalid-feedback">
+        Please provide a valid city.
+      </div>
+    </div>
+    
+    
+    <div class="col-md-3 mb-3">
+      <label for="validationCustom05">Zip</label>
+      <input type="text" class="form-control" id="validationCustom05" placeholder="Zip" required>
+      <div class="invalid-feedback">
+        Please provide a valid zip.
+      </div>
+    </div>
+    <div class="col-md-3 mb-3">
+
+    <label class="my-1 mr-2" for="inlineFormCustomSelectPref">Number of passengers</label>
+  <select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
+    <option selected>.</option>
+    <option value="1">One</option>
+    <option value="2">Two</option>
+    <option value="3">Three</option>
+  </select>
+  </div>
+  </div>
+  <div class="form-group">
+  <div class="col-md-3 mb-3">
+
+<label class="my-1 mr-2" for="inlineFormCustomSelectPref">le prix à payer est de : <strong>300 euros</strong>  </label>
+
+</div>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
+      <label class="form-check-label" for="invalidCheck">
+        Agree to terms and conditions
+      </label>
+      <div class="invalid-feedback">
+        You must agree before submitting.
+      </div>
+    </div>
+  </div>
+  <button class="btn btn-primary" type="submit">Submit form</button>
+</form>
+
+<script>
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+</script>
+        </div>  
+      </div>
+      
     </section><!-- End Featured Services Section -->
 
     <!-- ======= About Section ======= -->
     
 
-  </main><!-- End #main -->
 
-  <!-- ======= Footer =======
+ <!--  ======= Footer ======= 
   <footer id="footer">
 
     <div class="footer-newsletter">
@@ -227,10 +294,10 @@
         <!-- You can delete the links only if you purchased the pro version. -->
         <!-- Licensing information: https://bootstrapmade.com/license/ -->
         <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/bizland-bootstrap-business-template/ -->
-      <!--   Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      <!--  Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
       </div>
     </div>
-  </footer>End Footer --> 
+  </footer> End Footer --> -->
 
   <div id="preloader"></div>
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InfoVolController;
-
+use App\Http\Controllers\reserver;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,3 +20,5 @@ Route::get('/', function () {
 
 Route::get('/InfoVol',[InfoVolController::class,'index'])
                  ->name('InfoVol');
+Route::get('/Réserver',[reserver::class,'index'])
+                 ->name('réserver');
