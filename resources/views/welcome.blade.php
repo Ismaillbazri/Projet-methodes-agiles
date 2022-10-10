3babaf7b78b6b8a37112787b0a117790948dc595
Loubna HAFIDI <loubnahafidi9@gmail.com>
2022-10-10 16:08:00 +0100

    tables6

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -210,20 +210,20 @@
         &copy; Copyright <strong><span>BizLand</span></strong>. All Rights Reserved
       </div>
       <div class="credits">
-        <! -- All the links in the footer should remain intact. -->
+        <!-- All the links in the footer should remain intact. -->
         <!-- You can delete the links only if you purchased the pro version. -->
         <!-- Licensing information: https://bootstrapmade.com/license/ -->
         <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/bizland-bootstrap-business-template/ -->
       <!--  Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
       </div>
     </div>
-
-  </footer><! -- End Footer --
+<<<<<<< HEAD
+  </footer><!-- End Footer -->
   
+=======
+  </footer> End Footer --> -->
 
-  </footer> End Footer --> 
-
-
+>>>>>>> 763af2397981791667fb15e0f16b95078336c53e
   <div id="preloader"></div>
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
